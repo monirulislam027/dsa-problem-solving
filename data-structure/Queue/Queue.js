952a4b15539d7958b8aba23e7de84a6baa99dd4d
Monirul Islam <Md Monirul Islam>
2023-01-06 14:45:39 +0600

    qeuee data structure

---
+++ data-structure/Queue/Queue.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+  dequeue() {
+    if (!this.head) return null;
+    const data = this.head.value;
+    this.head = this.head.next;
+    return data;
+  }
+  isEmpty() {
+    return !!!this.head;
+  }
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(10);
+console.log('===');
+queue.print();
